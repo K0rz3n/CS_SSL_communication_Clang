18c9f198d6e240e05cd71de27d55a0a9488f53e3
k0rz3n <32642947+K0rz3n@users.noreply.github.com>
2024-10-09 13:07:19 +0100

    delete some files

--- openssl_c/build/openssl_cs.build/Debug/openssl_c.build/Objects-normal/arm64/socket_c.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/k0rz3n/projects/CProjects/openssl_cs/openssl_c/socket_c.c \
-  /Users/k0rz3n/projects/CProjects/openssl_cs/openssl_c/socket_c.h
--- openssl_s/build/openssl_cs.build/Debug/openssl_s.build/Objects-normal/arm64/socket_s-631f3547b40c4b1aa008295240a56f66.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/k0rz3n/projects/CProjects/openssl_cs/openssl_s/socket_s.c \
-  /Users/k0rz3n/projects/CProjects/openssl_cs/openssl_s/socket_s.h
